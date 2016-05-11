703d98498865596df6d201016a01b623a15e4377
Jordan Skole <jordan@usebenchmark.com>
2016-05-10 22:18:05 -0400

    do we need webhooks anymore or can we use the interface

--- README.md
+++ README.md
@@ -4,15 +4,16 @@ Ray the talking plant lets you know when it is thirsty, if the temperature is ou
 
 ## Parts List
 
-- Particle Photon ([particle](https://store.particle.io/collections/photon), [Sparkfun](https://www.sparkfun.com/products/13774), Adafruit)
-- Photon Weather Shield (sparkfun)
-- Soil Moisture Sensor (sparkfun)
+- Particle Photon ([particle](https://store.particle.io/collections/photon), [Sparkfun](https://www.sparkfun.com/products/13774), [Adafruit](https://www.adafruit.com/product/2721))
+- Photon Weather Shield ([sparkfun](https://www.sparkfun.com/products/13630))
+- Soil Moisture Sensor ([sparkfun](https://www.sparkfun.com/products/13322))
+- Optional: 3 prong, jst connector
 
 ## Pre-requisites
 
-- Particle Account
-- Particle CLI Software
-- Twilio Account
+- [Particle Account](https://build.particle.io/signup)
+- [Particle CLI Software](https://www.npmjs.com/package/particle-cli)
+- [Zapier Account](https://zapier.com/app/explore)
 
 #### Particle Account
 
---
+++ code-examples/final-sketch.c
@@ -0,0 +1,102 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "SparkFun_Photon_Weather_Shield_Library/SparkFun_Photon_Weather_Shield_Library.h"
+
+char Org[] = "The Iron Yard";
+char Disp[] = "Ray the Plant";
+char Locn[] = "Detroit, MI";
+
+// we need some time variables to be way easier
+int second = 1000;
+int minute = 60 * second;
+int hour = 60 * minute;
+int day = 24 * hour;
+
+// Stuff for the soil moisture sensor
+int soilPower = D5;
+int soilMoisture = A1;
+int moistureReading = 0;
+int soggyThreshold = 3400;
+int thirstyThreshold = 2900;
+
+
+//Create Instance of HTU21D or SI7021 temp and humidity sensor and MPL3115A2 barrometric sensor
+Weather sensor;
+
+void setup()
+
+{
+
+  Serial.begin(9600);
+
+  //Initialize the I2C sensors and ping them
+  sensor.begin();
+
+  /*You can only receive acurate barrometric readings or acurate altitiude
+  readings at a given time, not both at the same time. The following two lines
+  tell the sensor what mode to use. You could easily write a function that
+  takes a reading in one made and then switches to the other mode to grab that
+  reading, resulting in data that contains both acurate altitude and barrometric
+  readings. For this example, we will only be using the barometer mode. Be sure
+  to only uncomment one line at a time. */
+
+  sensor.setModeBarometer();//Set to Barometer Mode
+
+  // baro.setModeAltimeter();//Set to altimeter Mode
+  // These are additional MPL3115A2 functions the MUST be called for the sensor to work.
+  sensor.setOversampleRate(7); // Set Oversample rate
+  // Call with a rate from 0 to 7. See page 33 for table of ratios.
+  // Sets the over sample rate. Datasheet calls for 128 but you can set it
+  // from 1 to 128 samples. The higher the oversample rate the greater
+  // the time between data samples.
+
+  // set our soil moisture probe power pin as an output
+  pinMode(soilPower, OUTPUT);
+
+  // and then set the pin mode to low to preserve the life of the sensor
+  digitalWrite(soilPower, LOW);
+
+  // setup our cloud variable
+  Particle.variable("soil", moistureReading);
+
+  Serial.println("Device ready. Booting up...");
+
+  delay(10000);
+  Serial.println("Booted.");
+}
+
+void loop()
+
+{
+
+
+  // Measure Relative Humidity from the HTU21D or Si7021
+  float h = sensor.getRH();
+
+  // Measure Temperature from the HTU21D or Si7021
+  float f = sensor.getTempF();
+  // Temperature is measured every time RH is requested.
+  // It is faster, therefore, to read it from previous RH
+  // measurement with getTemp() instead with readTemp()
+
+  // power the soil moisture sensor just to get a reading
+  digitalWrite(soilPower, HIGH);
+
+  // take a soil moisture reading
+  moistureReading = analogRead(soilMoisture);
+
+  // and now power it down again to preserve the longevity of the sensor
+  digitalWrite(soilPower, LOW);
+  Serial.printlnf("Soil moisture is %i", moistureReading);
+
+
+  // publish the event
+  char payload[255];
+  snprintf(payload, sizeof(payload), "{ \"plant_name\":\"Maggy\", \"moisture\":\"%i\" }", moistureReading);
+  Particle.publish("plant-status", payload);
+
+
+  // wait for 12 hours to do it all again
+  delay(12 * hour);
+
+
+}
