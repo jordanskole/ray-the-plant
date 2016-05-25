a5e20f5b8ffd3337026fcde8c945acc267c7d6b9
Jordan Skole <jordan@usebenchmark.com>
2016-05-25 14:46:45 -0400

    some extra stuff

--- code-examples/starting-sketch.c
+++ code-examples/starting-sketch.c
@@ -1,32 +1,34 @@
 // Stuff for the soil moisture sensor
-int soilPower = D5;
-int soilMoisture = A1;
+int soilPowerPin = D5; // whatever pin is powering the soil moisture sensor
+int soilMoisturePin = A1; // whatever pin is reading the soil moisture
 
+// runs through once, right when the photon turns on
 void setup()
 
 {
 
   // set our soil moisture probe power pin as an output
-  pinMode(soilPower, OUTPUT);
+  pinMode(soilPowerPin, OUTPUT);
 
   // and then set the pin mode to low to preserve the life of the sensor
-  digitalWrite(soilPower, LOW);
+  digitalWrite(soilPowerPin, LOW);
 
 }
 
+// runs through forever, as long as the photon is on
 void loop()
 
 {
 
   // power the soil moisture sensor just to get a reading
-  digitalWrite(soilPower, HIGH);
+  digitalWrite(soilPowerPin, HIGH);
 
   // take a soil moisture reading
   int moistureReading;
-  moistureReading = analogRead(soilMoisture);
+  moistureReading = analogRead(soilMoisturePin);
 
   // and now power it down again to preserve the longevity of the sensor
-  digitalWrite(soilPower, LOW);
+  digitalWrite(soilPowerPin, LOW);
 
 
   // Do some webhook stuff
